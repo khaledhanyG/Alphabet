11460e6b1e74fbae393877886d3c661d5e7eb7c1
Khaled Hany Genina <57587600+khaledhanyG@users.noreply.github.com>
2021-08-18 22:20:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+function aFun() {
+  document.getElementById('photo').src="images/a.jpg";
+}
+function bFun() {
+  document.getElementById('photo').src="images/b.jpg";
+}
+function cFun() {
+  document.getElementById('photo').src="images/c.jpg";
+}
+function dFun() {
+  document.getElementById('photo').src="images/d.jpg";
+}
+function eFun() {
+  document.getElementById('photo').src="images/e.jpg";
+}
+function fFun() {
+  document.getElementById('photo').src="images/f.jpg";
+}
+function gFun() {
+  document.getElementById('photo').src="images/g.JPG";
+}
+function hFun() {
+  document.getElementById('photo').src="images/h.JPG";
+}
+function iFun() {
+  document.getElementById('photo').src="images/i.JPG";
+}
+function jFun() {
+  document.getElementById('photo').src="images/j.JPG";
+}
+function kFun() {
+  document.getElementById('photo').src="images/k.JPG";
+}
+function lFun() {
+  document.getElementById('photo').src="images/l.JPG";
+}
+function mFun() {
+  document.getElementById('photo').src="images/m.JPG";
+}
+function nFun() {
+  document.getElementById('photo').src="images/n.JPG";
+}
+function oFun() {
+  document.getElementById('photo').src="images/o.JPG";
+}
+function pFun() {
+  document.getElementById('photo').src="images/p.JPG";
+}
+function qFun() {
+  document.getElementById('photo').src="images/q.JPG";
+}
+function rFun() {
+  document.getElementById('photo').src="images/r.JPG";
+}
+function sFun() {
+  document.getElementById('photo').src="images/s.JPG";
+}
+function tFun() {
+  document.getElementById('photo').src="images/t.JPG";
+}
+function uFun() {
+  document.getElementById('photo').src="images/u.JPG";
+}
+function vFun() {
+  document.getElementById('photo').src="images/v.JPG";
+}
+function wFun() {
+  document.getElementById('photo').src="images/w.JPG";
+}
+function xFun() {
+  document.getElementById('photo').src="images/x.JPG";
+}
+function yFun() {
+  document.getElementById('photo').src="images/y.JPG";
+}
+function zFun() {
+  document.getElementById('photo').src="images/z.JPG";
+}
